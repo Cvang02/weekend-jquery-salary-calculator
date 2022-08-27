7da97bc3a50b7724d2e3f42f183b05484ffaa9d8
Chameng Vang <Chameng.vang@outlook.com>
2022-08-27 16:05:14 -0500

    add some css stlye to make it look more readable and cleaner.

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ $(document).ready(onReady);
 // A 'Submit' button should collect the form information, store the information 
 function onReady() {
     $('#submit-button').on('click', addEmployees);
+    $(document).on('click', '.deleteButton', deleteEmployee);
 }
 
+
 let totalMonthly = 0;
+let numbersOfEmployees = [];
 
 function addEmployees () {
     // lists of the employees information. 
@@ -25,15 +28,20 @@ function addEmployees () {
         title: employeeTitle,
         annualSalary: Number(employeeAnnualSalary)
     }
-    console.log(newEmployees);
+    
+    numbersOfEmployees.push(newEmployees)
+    console.log(numbersOfEmployees);
 
     $('#tablebody').append(`
-        <tr>
+        <tr id="tableRollOutput">
             <td>${employeeName}</td>
             <td>${employeeLast}</td>
             <td>${employeeID}</td>
             <td>${employeeTitle}</td>
             <td>${employeeAnnualSalary}</td>
+            <td>
+                <button class='deleteButton'>Delete</button>
+            </td>
         </tr>
     `);
 
@@ -42,6 +50,22 @@ function addEmployees () {
     $('#totalMonthly').empty();
     $('#totalMonthly').append(`<h4 id="totalMonthly">Total Monthly: $ ${Number(totalMonthly)}</h4>`);
 
-
 } //end of addEmployee-function. 
  
+function deleteEmployee () {
+    $('#tableRollOutput').remove();
+    // $('#totalMonthly').empty();
+    // $('#totalMonthly').append(`<h4 id="totalMonthly">Total Monthly: $ ${Number(totalMonthly)}</h4>`);
+}
+
+// function total () {
+//     if (totalMonthly > 20000) {
+//         $('#totalMonthly').css('backtext-color', 'red');
+//     }
+//     else {}
+// };
+
+
+// Need to find a way to change background color when total month exceed 20,000.
+// research if I am using .css correctly. Look at code to make sure it runs corrently and 
+// the way it intends it to. 
